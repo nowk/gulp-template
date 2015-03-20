32f8ccb60400a96919b2abb44c2816736f4553f9
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-03-20 16:47:49 -0400

    Add gulp-go setup

--- gulpfile.js
+++ gulpfile.js
@@ -83,6 +83,38 @@ gulp.task("browserify", function(done) {
 });
 
 /*
+ * go runner
+ *
+ */
+
+var gulpgo = require("gulp-go");
+
+var go;
+
+function out(prefix) {
+  prefix = (prefix || "");
+  return function(data) {
+    console.log(prefix, data.toString());
+  };
+}
+
+gulp.task("go-run", function() {
+  go = gulpgo.run("main.go", [], {
+    cwd:       __dirname,
+    onStdout:  out(),
+    onStderr:  out("[error]"),
+    onClose:   out("close"),
+    onExit:    out("exit")
+  });
+});
+
+gulp.task("devs", ["go-run", "watch"], function() {
+  gulp.watch([__dirname+"/**/*.go"]).on("change", function() {
+    go.restart();
+  });
+});
+
+/*
  * watch tasks
  *
  */
